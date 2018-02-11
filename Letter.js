c17de0635738b4831ebbfd93d609ae59677807dd
mjsimon91 <mjsimon91@gmail.com>
2018-02-10 23:07:23 -0500

    Adding rhe gitIgnore

--- Letter.js
+++ Letter.js
@@ -1,6 +1,9 @@
 // Contains a constructor, Letter. This constructor should be able to either display an underlying character or a blank placeholder (such as an underscore), depending on whether or not the user has guessed the letter. That means the constructor should define:
-
+function Letter(character,guessed){
 // A string value to store the underlying character for the letter
+  this.character = character
 // A boolean value that stores whether that letter has been guessed yet
+  this.guessed = guessed
+};
 // A function that returns the underlying character if the letter has been guessed, or a placeholder (like an underscore) if the letter has not been guessed
 // A function that takes a character as an argument and checks it against the underlying character, updating the stored boolean value to true if it was guessed correctly
